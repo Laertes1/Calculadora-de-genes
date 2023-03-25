0ad99347b49407c707f2d1f74023e1dd78664dc2
Laertes1 <laertes.angelo@gmail.com>
2023-03-25 14:08:57 -0300

    adicionei o inicio e a lógica

---
+++ QuadroDePunnet/inicio/script.js
@@ -0,0 +1,25 @@
+'use strict'
+
+
+setTimeout(() => {
+    document.querySelector('.titulo1').style.opacity="0"
+}, 1000);
+
+setTimeout(() => {
+     document.querySelector('.titulo2').style.opacity="1"
+}, 3000);
+
+setTimeout(() => {
+     document.querySelector('.titulo2').style.opacity="0"
+}, 4000);
+
+setTimeout(() => {
+    document.querySelector('.titulo3').style.opacity="1"
+}, 7000);
+setTimeout(() => {
+    document.querySelector('.titulo3').style.opacity="0"
+}, 9000);
+setTimeout(() => {
+    document.querySelector('.escolhaDosGenes').style.opacity="1"
+}, 12000);
+
